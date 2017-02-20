139bad6d057d931afbfb6ee4c3a26abfaa774b20
somedayrainbows <ebassity@gmail.com>
2017-02-20 14:06:44 -0700

    updated files

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,38 +1,55 @@
 require "csv"
-
+require 'pry'
 require 'sunlight/congress'
+require 'erb'
 
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
-
-def clean_zipcode(zipcode)
-  zipcode.to_s.rjust(5,"0")[0..4]
-end
-
-def legislators_by_zipcode(zipcode)
-  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
-  legislator_names = legislators.collect do |legislator|
-    "#{legislator.first_name} #{legislator.last_name}"
+  def clean_zipcode(zipcode)
+    zipcode.to_s.rjust(5,"0")[0..4]
   end
-  legislator_names.join(", ")
- end
 
+  # def clean_phones(home_phone)
+  #   home_phone.gsub!(/[^0-9A-Za-z]/, '')
+  #
+  #   # .to_s.rjust(10)[0..9]
+  #
+  # end
 
+  def legislators_by_zipcode(zipcode)
+    Sunlight::Congress::Legislator.by_zipcode(@zipcode)
+  end
+
+  def save_thank_you_letters(id,form_letter)
+    Dir.mkdir("output") unless Dir.exists? "output"
+    filename = "output/thanks_#{id}.html"
+    File.open(filename, 'w') do |file|
+      file.puts form_letter
+    end
+  end
 
 puts "EventManager Initialized."
 
 contents = CSV.open "event_attendees.csv",
 headers: true, header_converters: :symbol
 
-template_letter = File.read "form_letter.html"
+template_letter = File.read "form_letter.erb"
+erb_template = ERB.new template_letter
 
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
-  zipcode = clean_zipcode(row[:zipcode])
-  legislators = legislators_by_zipcode(zipcode).join(", ")
 
-  personal_letter = template_letter.gsub("FIRST_NAME",name)
-  personal_letter.gsub!
-  ("LEGISLATORS",legislators)
-  puts personal_letter
-end
+  home_phone = row[:homephone]
+  home_phone.replace "" if home_phone.length < 10
+  clean_phone = home_phone.gsub(/[^0-9A-Za-z]/, '')
+  if clean_phone.length > 10
+    clean_phone.reverse.chop.reverse 
+  end
+  p clean_phone
+
+  zipcode = clean_zipcode(row[:zipcode])
+  legislators = legislators_by_zipcode(zipcode)
+  form_letter = erb_template.result(binding)
+  save_thank_you_letters(id,form_letter)
+  end
